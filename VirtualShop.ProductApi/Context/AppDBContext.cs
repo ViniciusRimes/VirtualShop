ed5cec4c64527a700d16289298a2551f9b41828e
Vinicius Rimes de Oliveira <viniciusrimess@gmail.com>
2024-04-12 16:36:24 -0300

    Create project and database

---
+++ VirtualShop.ProductApi/Context/AppDBContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualShop.ProductApi.Models;
+
+namespace VirtualShop.ProductApi.Context;
+
+public class AppDBContext : DbContext
+{
+    public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    protected override void OnModelCreating(ModelBuilder mb)
+    {
+        mb.Entity<Category>().HasKey(c => c.Id);
+        mb.Entity<Category>()
+            .Property(c => c.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+        
+        mb.Entity<Category>()
+            .HasMany(c => c.Products)
+            .WithOne(c => c.Category)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        mb.Entity<Category>().HasData(
+        new Category
+        {
+            Id = 1,
+            Name = "Acessórios"
+        },
+        new Category 
+        {
+            Id = 2,
+            Name = "Material Escolar"
+        }
+        );
+
+        mb.Entity<Product>().HasKey(p => p.Id);
+        mb.Entity<Product>()
+            .Property(p => p.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        mb.Entity<Product>()
+            .Property(p => p.Description)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        mb.Entity<Product>()
+            .Property(p => p.ImageURL)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        mb.Entity<Product>()
+            .Property(p => p.Price)
+            .HasPrecision(12,2)
+            .IsRequired();
+
+    }
+}
---
+++ VirtualShop.ProductApi/Migrations/20240412192115_InsertProducts.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VirtualShop.ProductApi.Migrations
+{
+    public partial class InsertProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, ImageURL, CategoryId) " +
+                "VALUES('Caderno Flamengo 200 folhas', 'Caderno Flamengo 200 folhas', 30, 100, 'caderno+flamengo.png', 2)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, ImageURL, CategoryId) " +
+                "VALUES('Caderno Vasco 200 folhas', 'Caderno Vasco 200 folhas', 25, 100, 'caderno+vasco.png', 2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
